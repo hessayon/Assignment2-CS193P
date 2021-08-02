8df3dc2e8f7450b2c5b465a3940a961c9fd7f77b
hessayon <imaggi99@mail.ru>
2021-08-02 16:40:15 +0300

    Initial Commit

---
+++ Assignment2-CS193P/Assignment2_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2_CS193PApp.swift
+//  Assignment2-CS193P
+//
+//  Created by Margo Naumenko on 02.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2-CS193P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment2-CS193P
+//
+//  Created by Margo Naumenko on 02.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
