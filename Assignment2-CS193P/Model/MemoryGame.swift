f7f3304df7bab50c13fac856344456f1b82651a5
hessayon <imaggi99@mail.ru>
2021-08-03 20:32:59 +0300

    Make Extra Credit 3 and 4

--- Assignment2-CS193P/Model/MemoryGame.swift
+++ Assignment2-CS193P/Model/MemoryGame.swift
@@ -15,21 +15,28 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     private var indexOfTheOnlyOneFaceUpCard: Int?
     
+    private var timeWhenTheOnlyOneFaceUpCardWasChosen: Date? // Extra credit 4
+    
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}),
            !card.isFaceUp,
            !card.isMathed{
-            if let potentialMatchIndex = indexOfTheOnlyOneFaceUpCard {
+            if let potentialMatchIndex = indexOfTheOnlyOneFaceUpCard,
+               let timeWhenCardWasChosen = timeWhenTheOnlyOneFaceUpCardWasChosen {
+                let numOfSecondsSinceCardWasChosen = Date().timeIntervalSince(timeWhenCardWasChosen)
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                     cards[chosenIndex].isMathed = true
                     cards[potentialMatchIndex].isMathed = true
-                    score += 2 // Required Task 15
+                    //score += 2 // Required Task 15
+                    score += max(10 - Int(numOfSecondsSinceCardWasChosen), 1) * 2 // Extra Credit 4
                 } else if cards[chosenIndex].hasBeenSeen || cards[potentialMatchIndex].hasBeenSeen {
-                    score -= 1
+                    //score -= 1 // Required Task 15
+                    score += min(10 - Int(numOfSecondsSinceCardWasChosen), -1) // Extra Credit 4
                 }
                 indexOfTheOnlyOneFaceUpCard = nil
             } else {
                 indexOfTheOnlyOneFaceUpCard = chosenIndex
+                timeWhenTheOnlyOneFaceUpCardWasChosen = Date()
                 for index in cards.indices {
                     if cards[index].isFaceUp {
                         cards[index].isFaceUp = false
