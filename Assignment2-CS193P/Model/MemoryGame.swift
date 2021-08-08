bb3b4868921d78bc9a055040bc9e2d99fa098844
hessayon <imaggi99@mail.ru>
2021-08-08 20:04:29 +0300

    Change score counting logic

--- Assignment2-CS193P/Model/MemoryGame.swift
+++ Assignment2-CS193P/Model/MemoryGame.swift
@@ -31,7 +31,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
                     score += max(10 - Int(numOfSecondsSinceCardWasChosen), 1) * 2 // Extra Credit 4
                 } else if cards[chosenIndex].hasBeenSeen || cards[potentialMatchIndex].hasBeenSeen {
                     //score -= 1 // Required Task 15
-                    score += min(10 - Int(numOfSecondsSinceCardWasChosen), -1) // Extra Credit 4
+                    score += max(10 - Int(numOfSecondsSinceCardWasChosen), 1) * -1 // Extra Credit 4
                 }
                 indexOfTheOnlyOneFaceUpCard = nil
                 timeWhenTheOnlyOneFaceUpCardWasChosen = nil
